a85ac971bb4f09b8220c35fefc439d1b70352e4d
gioinsfran1998 <gioinsfran98@gmail.com>
2023-07-05 17:09:29 -0300

    refactor(clock): add function to start animated

---
+++ updateTime.ts
@@ -0,0 +1,13 @@
+function updateTime() {
+  //   const date = new Date();
+  //   const hours = date.getHours();
+  //   const minutes = date.getMinutes();
+  //   const seconds = date.getSeconds();
+  //   const calculateHour = getHoursAngle(hours);
+  //   const caculateMinutes = getMinutesAngle(minutes);
+  //   const calculateSeconds = getSecondsAngle(seconds);
+  //   pointerHour.rotation = calculateHour;
+  //   pointerMinute.rotation = caculateMinutes;
+  //   pointerSecond.rotation = calculateSeconds;
+  //   text.text = `${hours}:${minutes}:${seconds}`;
+}
